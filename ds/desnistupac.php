b5d983d162ed50f42bc14a5a55dc49d2310d9d21
Deanunipu <dparencan@unipu.hr>
2016-11-29 17:52:02 +0100

    Add files via upload

---
+++ ds/desnistupac.php
@@ -0,0 +1,8 @@
+<div id="desnistupac">
+        <a class="button add" href="../Kvar/indexkvar.php"> Kvarovi</a>
+        <a class="button add" href="../Status/indexstat.php"> Statusi</a>
+        <a class="button add" href="../Smjestaj/indexsmjestaj.php"> Smjestaj</a>
+        <a class="button add" href="../Lokacija/indexlokacija.php"> Lokacija</a>
+        <a class="button add" href="../Korisnik/indexkorisnik.php"> Korisnici</a>
+
+    </div>
---
+++ ds/header.php
@@ -0,0 +1,10 @@
+
+
+
+
+
+<div id="zaglavlje"></div>
+
+
+
+
