1e0ecdaa30c74c7636d3b28ea1e57e848ce17917
Deanunipu <dparencan@unipu.hr>
2017-01-11 14:59:30 +0100

    Delete db_connect.php

--- Status/php/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection = mysqli_connect('localhost', 'root', '');
-if (!$connection){
-    die("Spajanje na bazu neuspijesno !!!" . mysqli_error($connection));
-}
-$select_db = mysqli_select_db($connection, 'db');
-if (!$select_db){
-    die("Odabir baze neuspijesan !!!" . mysqli_error($connection));
-}
